5ef7c6f10c4037c918f5279af67eca7ef67a1b3c
George Sydby <george.sydby@gmail.com>
2021-11-08 15:14:26 +0100

    Tror det är klart men man vet aldrig.

--- WebAppDemo/Controllers/HomeController.cs
+++ WebAppDemo/Controllers/HomeController.cs
@@ -14,20 +14,20 @@ namespace WebAppDemo.Controllers
         }
         public ActionResult About()
         {
-            ViewBag.Message = "Jag chansar och gissar att detta är en controller för About." +
-                              "Containing information about yourself (CV, for example).";
+            /*ViewBag.Message = "Jag chansar och gissar att detta är en controller för About.\n" +
+                              "Containing information about yourself (CV, for example).";*/
             return View();
         }
         public ActionResult Contact()
         {
-            ViewBag.Message = "Jag chansar och gissar att detta är en controller för Contact." +
-                              "Containing your contact information (Use fake info if you want).";
+            /*ViewBag.Message = "Jag chansar och gissar att detta är en controller för Contact.\n" +
+                              "Containing your contact information (Use fake info if you want).";*/
             return View();
         }
         public ActionResult Projects()
         {
-            ViewBag.Message = "Containing the GitHub links to your assignments you have finished with " +
-                              "a small description about them.";
+            /*ViewBag.Message = "Containing the GitHub links to your assignments you have finished with " +
+                              "a small description about them.";*/
             return View();
         }
     }
