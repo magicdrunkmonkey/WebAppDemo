cbe9812f8bbeebb26288bb83f1d24ff94086cebd
George Sydby <george.sydby@gmail.com>
2021-11-08 11:43:19 +0100

    Färdig med Guide to making an “Empty” ASP.NET Core MVC project.och verkar fungera

--- WebAppDemo/Startup.cs
+++ WebAppDemo/Startup.cs
@@ -16,6 +16,7 @@ namespace WebAppDemo
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddMvc();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -26,14 +27,22 @@ namespace WebAppDemo
                 app.UseDeveloperExceptionPage();
             }
 
+            app.UseStaticFiles();
+
             app.UseRouting();
 
             app.UseEndpoints(endpoints =>
             {
+                endpoints.MapControllerRoute(
+                    name: "default",
+                    pattern: "{controller=Home}/{action=Index}/{id?}"
+                );
+
+                /* Så som det stod innan ändring till MapControllerRoute()
                 endpoints.MapGet("/", async context =>
                 {
                     await context.Response.WriteAsync("Hello World!");
-                });
+                });*/ 
             });
         }
     }
