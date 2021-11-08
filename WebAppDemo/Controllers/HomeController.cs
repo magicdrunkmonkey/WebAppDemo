96599a09df08dde0423bdfff52972fcbdeb8f817
George Sydby <george.sydby@gmail.com>
2021-11-08 13:45:14 +0100

    Är kanske klar med uppgift del 1 av 3

--- WebAppDemo/Controllers/HomeController.cs
+++ WebAppDemo/Controllers/HomeController.cs
@@ -12,5 +12,23 @@ namespace WebAppDemo.Controllers
         {
             return View();
         }
+        public ActionResult About()
+        {
+            ViewBag.Message = "Jag chansar och gissar att detta är en controller för About." +
+                              "Containing information about yourself (CV, for example).";
+            return View();
+        }
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Jag chansar och gissar att detta är en controller för Contact." +
+                              "Containing your contact information (Use fake info if you want).";
+            return View();
+        }
+        public ActionResult Projects()
+        {
+            ViewBag.Message = "Containing the GitHub links to your assignments you have finished with " +
+                              "a small description about them.";
+            return View();
+        }
     }
 }
