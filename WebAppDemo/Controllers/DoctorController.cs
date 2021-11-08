4385fe0554cb7d4da7fbc1995f0a193a7783884f
George Sydby <george.sydby@gmail.com>
2021-11-08 15:39:02 +0100

    Provar på deluppgift 2 av 3

---
+++ WebAppDemo/Controllers/DoctorController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebAppDemo.Controllers
+{
+    public class DoctorController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult FeverCheck()
+        {
+            return View();
+        }
+    }
+}
