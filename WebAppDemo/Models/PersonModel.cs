a66146d00451d98a28f5947bbe204414453a1659
George Sydby <george.sydby@gmail.com>
2021-11-15 10:52:35 +0100

    Skapat några get och set i Personmodel och ser sen vad det leder.

--- WebAppDemo/Models/PersonModel.cs
+++ WebAppDemo/Models/PersonModel.cs
@@ -5,7 +5,15 @@ using System.Threading.Tasks;
 
 namespace WebAppDemo.Models
 {
+    /*
+     * Create a MVC project with a controller with one view to displays a list of people.
+     * These people should have a name, phone number and city
+     * public class PersonModel
+     */
     public class PersonModel
     {
+        public string Name { get; set; }
+        public int PhoneNumber { get; set; }
+        public string City { get; set; }
     }
 }
