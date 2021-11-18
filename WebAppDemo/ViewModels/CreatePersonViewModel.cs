906b1d0670b4a64f361742eea874c776a5a1ead4
George Sydby <george.sydby@gmail.com>
2021-11-18 08:47:18 +0100

    Update

--- WebAppDemo/ViewModels/CreatePersonViewModel.cs
+++ WebAppDemo/ViewModels/CreatePersonViewModel.cs
@@ -7,5 +7,15 @@ namespace WebAppDemo.ViewModels
 {
     public class CreatePersonViewModel
     {
+        /*Code Requirements:
+         * • Models
+         *      o Person – Person data.
+         *      o CreatePersonViewModel – Use to prevent overposting and to use data
+         *        annotations to validate inputs when creating new person.
+         *      o PeopleViewModel – container for the information you need in your people view.
+         *  • The table data should come from a view model, which should have a list of people, and
+         *    the search phrase if one exists.
+         */
+
     }
 }
